87b4ac849559b121105e8732723988e330bcf924
Gong Zhang <zhanggong@me.com>
2017-02-28 09:51:24 +0800

    Add Java example

---
+++ Java/ProcBridge/Sample/src/co/gongzh/procbridge/samples/SampleClient.java
@@ -0,0 +1,34 @@
+package co.gongzh.procbridge.samples;
+
+import co.gongzh.procbridge.ProcBridge;
+import co.gongzh.procbridge.ProcBridgeException;
+import org.json.JSONObject;
+
+/**
+ * @author Gong Zhang
+ */
+public class SampleClient {
+
+    public static void main(String[] args) {
+
+        String host = "127.0.0.1";
+        int port = 8877;
+        long timeout = 10000; // 10 seconds
+
+        ProcBridge pb = new ProcBridge(host, port, timeout);
+
+        try {
+            JSONObject resp;
+
+            resp = pb.request("echo", "{}");
+            System.out.println(resp);
+
+            resp = pb.request("add", "{elements: [1, 2, 3, 4, 5]}");
+            System.out.println(resp);
+
+        } catch (ProcBridgeException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
